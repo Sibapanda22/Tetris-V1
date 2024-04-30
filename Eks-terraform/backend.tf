b4e4bf4b5e15047a7e88c5079254ef70f5fdd6cc
Sibapanda22 <95164232+Sibapanda22@users.noreply.github.com>
2024-04-30 22:44:27 +0530

    Update backend.tf
    backend key file name change

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "sibapanda-s3bucket" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "Jenkins-terraform-eks.tfstate"
     region = "ap-south-1"
   }
 }
